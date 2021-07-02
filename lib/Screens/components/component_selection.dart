893b506da953c932194da49075e81b6ca15df5da
Zachary Wauer <41166066+Z-inator@users.noreply.github.com>
2021-07-02 11:06:36 -0500

    Working on first UI approach

--- lib/Screens/components/component_selection.dart
+++ lib/Screens/components/component_selection.dart
@@ -1,47 +1,18 @@
 import 'package:flutter/material.dart';
 import 'package:star_citizen_app/Screens/components/component_tile.dart';
 
-class ComponentSelection extends StatelessWidget {
-  const ComponentSelection({Key? key}) : super(key: key);
+class ComponentSelectionList extends StatelessWidget {
+  const ComponentSelectionList({Key? key}) : super(key: key);
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      color: Theme.of(context).colorScheme.primary,
-      child: Column(
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
-        mainAxisSize: MainAxisSize.max,
-        children: [
-          ComponentTile(),
-          ListTile(
-            title: Text('Build Name'),
-          ),
-          ListTile(
-            title: Text('Weapons'),
-          ),
-          ListTile(
-            title: Text('Turrets'),
-          ),
-          ListTile(
-            title: Text('Missiles'),
-          ),
-          ListTile(
-            title: Text('EMPS'),
-          ),
-          ListTile(
-            title: Text('Shields'),
-          ),
-          ListTile(
-            title: Text('Power'),
-          ),
-          ListTile(
-            title: Text('Cooling'),
-          ),
-          ListTile(
-            title: Text('EM/IR'),
-          ),
-        ],
-      ),
-    );
+      child: ListView.builder(
+          itemBuilder: (context, index) => ComponentTile(
+              componentTitle: ListTile(
+                title: Text(components[index]),
+              ),
+              componentItems: weapons,
+    )));
   }
 }
--- lib/Screens/components/component_tile.dart
+++ lib/Screens/components/component_tile.dart
@@ -1,83 +1,262 @@
 import 'package:flutter/material.dart';
 
+List<String> components = [
+  'Weapons',
+  'Turrets',
+  'Missiles',
+  'EMPS',
+  'QEDS',
+  'Utilities',
+  'Shields',
+  'Power Plants',
+  'Coolers',
+  'Quantum Drive',
+  'Radars',
+  'Thrusters',
+];
+
+List<String> weapons = ['Revenant', 'Revenant'];
 
 class ComponentTile extends StatelessWidget {
-  const ComponentTile({Key? key}) : super(key: key);
+  const ComponentTile(
+      {Key? key, required this.componentTitle, required this.componentItems})
+      : super(key: key);
+
+  final Widget componentTitle;
+  final List<String> componentItems;
 
   @override
   Widget build(BuildContext context) {
     return Container(
-      height: 100.0,
-      padding: EdgeInsets.all(10),
+        padding: EdgeInsets.all(10),
+        child: Material(
+          type: MaterialType.card,
+          shape: BeveledRectangleBorder(
+            borderRadius: BorderRadius.all(Radius.circular(8.0)),
+          ),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.stretch,
+            children: [
+            componentTitle,
+            ListView(
+              shrinkWrap: true,
+              children: componentItems
+                  .map((name) => ComponentRow())
+                  .toList(),
+            )
+          ]),
+        ));
+  }
+}
+
+class ComponentRow extends StatelessWidget {
+  const ComponentRow({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
       child: Row(
         mainAxisSize: MainAxisSize.max,
-        mainAxisAlignment: MainAxisAlignment.spaceAround,
         children: [
-          Material(
-            color: Theme.of(context).colorScheme.primaryVariant,
-            elevation: 4,
-            shape: BeveledRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(8.0)),
-            ),
-            child: Container(
-              width: 80.0,
-              child: Column(
-                children: [
-                  TextButton(
-                    child: Text('Gimbal S4'),
-                    onPressed: () {},
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    mainAxisSize: MainAxisSize.max,
-                    children: [
-                      Icon(Icons.bolt_rounded),
-                      Text('34'),
-                      Spacer(),
-                      Icon(Icons.ac_unit_rounded),
-                      Text('26')
-                    ],
-                  )
-                ],
-              ),
-            ),
-          ),
+          Expanded(
+            flex: 1,
+            child: ComponentSize()),
           Container(
-            width: 24.0,
             height: 2.0,
+            width: 20.0,
             color: Theme.of(context).colorScheme.secondary,
           ),
-          Material(
-            color: Theme.of(context).colorScheme.primaryVariant,
-            elevation: 4,
-            shape: BeveledRectangleBorder(
-                borderRadius: BorderRadius.all(Radius.circular(8.0)),
-            ),
-            child: Container(
-              width: 80.0,
-              child: Column(
-                children: [
-                  TextButton(
-                    child: Text('Revenant'),
-                    onPressed: () {},
-                  ),
-                  Row(
-                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                    mainAxisSize: MainAxisSize.max,
-                    children: [
-                      Icon(Icons.bolt_rounded),
-                      Text('34'),
-                      Spacer(),
-                      Icon(Icons.ac_unit_rounded),
-                      Text('26')
-                    ],
-                  )
-                ],
-              ),
-            ),
-          ),
+          Expanded(
+            flex: 3,
+            child: ComponentItem())
         ],
       ),
     );
   }
 }
+
+class DetailRow extends StatelessWidget {
+  const DetailRow({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: null,
+    );
+  }
+}
+
+class ComponentSize extends StatelessWidget {
+  const ComponentSize({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          OutlinedButton(
+            child: Text('Gimbal S4'),
+            onPressed: () {},
+          ),
+          Row(
+            mainAxisSize: MainAxisSize.max,
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            children: [buildPower(), buildCooler()],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget buildPower() {
+    return Row(
+      mainAxisSize: MainAxisSize.max,
+      children: [Icon(Icons.bolt_sharp), Text('20')],
+    );
+  }
+
+  Widget buildCooler() {
+    return Row(
+      mainAxisSize: MainAxisSize.max,
+      children: [Icon(Icons.ac_unit_sharp), Text('34')],
+    );
+  }
+}
+
+class ComponentItem extends StatelessWidget {
+  const ComponentItem({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        mainAxisAlignment: MainAxisAlignment.center,
+        children: [
+          Container(
+            width: double.infinity,
+            child: OutlinedButton(
+              child: Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                mainAxisSize: MainAxisSize.max,
+                children: [
+                  Text('Revenant'),
+                  Text('972dps')
+                ],
+              ),
+            //   ListTile(
+            //     title: Text('Revenant'),
+            // trailing: Text('672dps'),
+            //   ),
+              onPressed: () {},
+            ),
+          ),
+          Row(
+            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+            mainAxisSize: MainAxisSize.max,
+            children: [buildPowerButton(), buildPowerAndCooler()],
+          )
+        ],
+      ),
+    );
+  }
+
+  Widget buildPowerAndCooler() {
+    return Row(
+      children: [buildPower(), buildCooler()],
+    );
+  }
+
+  Widget buildPowerButton() {
+    return Row(
+      children: [
+        TextButton(onPressed: () {}, child: Text('Power')),
+        TextButton(onPressed: () {}, child: Text('Overclock'))
+      ],
+    );
+  }
+
+  Widget buildPower() {
+    return Row(
+      children: [Icon(Icons.bolt_sharp), Text('283')],
+    );
+  }
+
+  Widget buildCooler() {
+    return Row(
+      children: [Icon(Icons.ac_unit_sharp), Text('41925')],
+    );
+  }
+}
+
+// Row(
+//         mainAxisSize: MainAxisSize.max,
+//         mainAxisAlignment: MainAxisAlignment.spaceAround,
+//         children: [
+//           Material(
+//             color: Theme.of(context).colorScheme.primaryVariant,
+//             elevation: 4,
+//             shape: BeveledRectangleBorder(
+//                 borderRadius: BorderRadius.all(Radius.circular(8.0)),
+//             ),
+//             child: Container(
+//               width: 80.0,
+//               child: Column(
+//                 children: [
+//                   TextButton(
+//                     child: Text('Gimbal S4'),
+//                     onPressed: () {},
+//                   ),
+//                   Row(
+//                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                     mainAxisSize: MainAxisSize.max,
+//                     children: [
+//                       Icon(Icons.bolt_rounded),
+//                       Text('34'),
+//                       Spacer(),
+//                       Icon(Icons.ac_unit_rounded),
+//                       Text('26')
+//                     ],
+//                   )
+//                 ],
+//               ),
+//             ),
+//           ),
+//           Container(
+//             width: 24.0,
+//             height: 2.0,
+//             color: Theme.of(context).colorScheme.secondary,
+//           ),
+//           Material(
+//             color: Theme.of(context).colorScheme.primaryVariant,
+//             elevation: 4,
+//             shape: BeveledRectangleBorder(
+//                 borderRadius: BorderRadius.all(Radius.circular(8.0)),
+//             ),
+//             child: Container(
+//               width: 80.0,
+//               child: Column(
+//                 children: [
+//                   TextButton(
+//                     child: Text('Revenant'),
+//                     onPressed: () {},
+//                   ),
+//                   Row(
+//                     mainAxisAlignment: MainAxisAlignment.spaceBetween,
+//                     mainAxisSize: MainAxisSize.max,
+//                     children: [
+//                       Icon(Icons.bolt_rounded),
+//                       Text('34'),
+//                       Spacer(),
+//                       Icon(Icons.ac_unit_rounded),
+//                       Text('26')
+//                     ],
+//                   )
+//                 ],
+//               ),
+//             ),
+//           ),
+//         ],
+//       ),
