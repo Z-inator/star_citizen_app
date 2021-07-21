dcf6e4ec3034b1a774df161eddea19ab81955152
Zachary Wauer <41166066+Z-inator@users.noreply.github.com>
2021-07-21 14:45:11 -0500

    First implementation of DataTable for Components

---
+++ lib/Screens/data_screen.dart
@@ -0,0 +1,69 @@
+import 'package:flutter/material.dart';
+import 'package:star_citizen_app/Models/weapon.dart';
+import 'package:table_sticky_headers/table_sticky_headers.dart';
+
+class ComponentDataTable extends StatelessWidget {
+  final List<Weapon> componentItems;
+  // final List<String> componentList;
+  // final List<String> componentAttributes;
+  final List<String> componentData;
+  ComponentDataTable(
+      {Key? key,
+      // required this.componentList,
+      // required this.componentAttributes,
+      required this.componentData,
+      required this.componentItems})
+      : super(key: key);
+
+  late List<String> componentList;
+  late List<String> componentAttributes;
+
+  List<String> buildColumn() {
+    List<String> column = [];
+    componentItems.map((weapon) {
+      column.add(weapon.name);
+    });
+    return column;
+  }
+
+  List<String> buildRow() {
+    List<String> row = [];
+    var tempItem = componentItems[0];
+    Map<String, dynamic> jsonMap = tempItem.toMap();
+    row.addAll(jsonMap.keys);
+    return row;
+  }
+
+  List<List<String>> buildData(int numberOfColumns, int numberOfRows) {
+    List<List<String>> output = [];
+    for (int i = 0; i < numberOfColumns; i++) {
+      List<String> row = [];
+      Map<String, dynamic> tempItem = componentItems[i].toMap();
+      List<dynamic> tempAttributes = tempItem.values.toList();
+      for (int j = 0; j < numberOfRows; j++) {
+        row.add(tempAttributes[j]);
+      }
+      output.add(row);
+    }
+    return output;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    componentList = buildColumn();
+    componentAttributes = buildRow();
+    return StickyHeadersTable(
+        columnsLength: componentList.length,
+        rowsLength: componentAttributes.length,
+        columnsTitleBuilder: (i) => Text(componentList[i]),
+        rowsTitleBuilder: (i) => Text(componentAttributes[i]),
+        contentCellBuilder: (i, j) => Text(
+            buildData(componentList.length, componentAttributes.length)[i][j]));
+  }
+}
+
+// Ship
+// i=0,j=0
+// Ship1, name
+// i=0,j=1
+// Ship1, Manufacterer
