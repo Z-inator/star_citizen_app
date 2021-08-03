aa0a07c6f9fdd70b16806fc4ebf80834ad394c6d
Zachary Wauer <41166066+Z-inator@users.noreply.github.com>
2021-08-03 11:02:11 -0500

    Added Firestore

--- lib/main.dart
+++ lib/main.dart
@@ -8,7 +8,7 @@ import 'package:star_citizen_app/Screens/mobile_screen.dart';
 import 'package:star_citizen_app/Services/api.dart';
 import 'package:star_citizen_app/constants.dart';
 import 'package:star_citizen_app/routes.dart';
-
+import 'package:firebase_core/firebase_core.dart';
 import 'Models/ship.dart';
 import 'Screens/widgets/component_selection.dart';
 import 'Screens/widgets/stats_dashboard.dart';
@@ -18,8 +18,53 @@ import 'Screens/widgets/stats_dashboard.dart';
 /// Regex(?<=[a-z])(?=[A-Z])|(?<=[A-Z])(?=[A-Z][a-z])
 /// eclipseRCPExt > eclipse/RCP/Ext
 
+import 'package:flutter/material.dart';
+
+// Import the firebase_core plugin
+
+
 void main() {
-  runApp(MyApp());
+  WidgetsFlutterBinding.ensureInitialized();
+  runApp(App());
+}
+
+/// We are using a StatefulWidget such that we only create the [Future] once,
+/// no matter how many times our widget rebuild.
+/// If we used a [StatelessWidget], in the event where [App] is rebuilt, that
+/// would re-initialize FlutterFire and make our application re-enter loading state,
+/// which is undesired.
+class App extends StatefulWidget {
+  // Create the initialization Future outside of `build`:
+  @override
+  _AppState createState() => _AppState();
+}
+
+class _AppState extends State<App> {
+  /// The future is part of the state of our widget. We should not call `initializeApp`
+  /// directly inside [build].
+  final Future<FirebaseApp> _initialization = Firebase.initializeApp();
+
+  @override
+  Widget build(BuildContext context) {
+    return FutureBuilder(
+      // Initialize FlutterFire:
+      future: _initialization,
+      builder: (context, snapshot) {
+        // Check for errors
+        if (snapshot.hasError) {
+          return Center(child: Text('Something went weong'),);
+        }
+
+        // Once complete, show your application
+        if (snapshot.connectionState == ConnectionState.done) {
+          return MyApp();
+        }
+
+        // Otherwise, show something whilst waiting for initialization to complete
+        return Center(child: CircularProgressIndicator(),);
+      },
+    );
+  }
 }
 
 class MyApp extends StatefulWidget {
